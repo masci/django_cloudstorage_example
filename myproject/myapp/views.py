04511fd7e5ff32ce83fc2f84a34fa199b1ba256d
Massimiliano Pippi <mpippi@gmail.com>
2014-04-13 17:15:27 +0200

    fixed urls and view name

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -7,25 +7,22 @@ from django.core.urlresolvers import reverse
 from myproject.myapp.models import Document
 from myproject.myapp.forms import DocumentForm
 
-def list(request):
+
+def list_docs(request):
     # Handle file upload
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
-            newdoc = Document(docfile = request.FILES['docfile'])
+            newdoc = Document(docfile=request.FILES['docfile'])
             newdoc.save()
 
             # Redirect to the document list after POST
             return HttpResponseRedirect(reverse('myproject.myapp.views.list'))
     else:
-        form = DocumentForm() # A empty, unbound form
+        form = DocumentForm()  # A empty, unbound form
 
     # Load documents for the list page
     documents = Document.objects.all()
 
     # Render list page with the documents and the form
-    return render_to_response(
-        'myapp/list.html',
-        {'documents': documents, 'form': form},
-        context_instance=RequestContext(request)
-    )
+    return render_to_response('myapp/list_docs.html', context_instance=RequestContext(request))
